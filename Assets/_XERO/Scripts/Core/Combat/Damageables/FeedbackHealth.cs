1a238511bd7b5b18244db391be15406da8eef8e3
Darnok <112723058+darnOK-dev@users.noreply.github.com>
2026-07-12 14:27:04 +0200

    health related feedbacks placeholders

--- Assets/_XERO/Scripts/Core/Combat/Damageables/FeedbackHealth.cs
+++ Assets/_XERO/Scripts/Core/Combat/Damageables/FeedbackHealth.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 public class FeedbackHealth : Health
 {
     [SerializeField] protected MMF_Player damageFeedback;
+    [SerializeField] protected MMF_Player healFeedback;
+    [SerializeField] protected MMF_Player deathFeedback;
+    [SerializeField] protected bool isDeathFeedbackStopDamageFeedback = true;
     [SerializeField] protected float damageCooldown = 0.25f;
 
     protected float lastDamageTakenAt = int.MinValue;
 
     public override void TakeDamage(DamageDataSO damageDataSO)
     {
+        if (damageDataSO.DamageAmount == 0)
+        {
+            Debug.LogWarning("<color=red>[FeedbackHealth]</color> Damage amount is 0, no feedback will be played.");
+            base.TakeDamage(damageDataSO);
+            return;
+        }
+        
         try
         {
             if (damageFeedback != null)
@@ -24,10 +34,45 @@ public class FeedbackHealth : Health
             {
                 Debug.LogError("<color=red>[FeedbackHealth]</color> BarbarianEnemy reference is not assigned.");
             }
+
+            if (healFeedback != null)
+            {
+                if (damageDataSO.DamageAmount < 0)
+                {
+                    healFeedback.PlayFeedbacks(transform.position, damageDataSO.DamageAmount);
+                }
+            }
+            else
+            {
+                Debug.LogWarning("<color=red>[FeedbackHealth]</color> BarbarianEnemy reference is not assigned.");
+            }
         }
         finally
         {
             base.TakeDamage(damageDataSO);
+        }
+    }
+
+    public override void Kill()
+    {
+        try
+        {
+            if (deathFeedback != null)
+            {
+                if (isDeathFeedbackStopDamageFeedback && damageFeedback != null)
+                {
+                    damageFeedback.StopFeedbacks();
+                }
+                deathFeedback.PlayFeedbacks(transform.position);
+            }
+            else
+            {
+                Debug.LogWarning("<color=red>[FeedbackHealth]</color> BarbarianEnemy reference is not assigned.");
+            }
+        }
+        finally
+        {
+            base.Kill();
         }
     }
 }
